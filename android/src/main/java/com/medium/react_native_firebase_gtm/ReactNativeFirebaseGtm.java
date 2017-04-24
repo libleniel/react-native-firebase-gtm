a4b5f64023498708b480bc0c4931f6131d2466dc
EwyynTomato <nkaloli@gmail.com>
2017-04-24 15:12:11 +0800

    Added methods for Android

--- android/src/main/java/com/medium/react_native_firebase_gtm/ReactNativeFirebaseGtm.java
+++ android/src/main/java/com/medium/react_native_firebase_gtm/ReactNativeFirebaseGtm.java
@@ -1,16 +1,26 @@
 package com.medium.react_native_firebase_gtm;
 
+import android.util.Log;
+
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
-
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 
 public class ReactNativeFirebaseGtm extends ReactContextBaseJavaModule{
+    private final String TAG = "firebase-gtm";
+
+    private FirebaseRemoteConfig mFirebaseRemoteConfig;
+
     public ReactNativeFirebaseGtm(ReactApplicationContext reactContext) {
         super(reactContext);
+
+        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
     }
 
     @ReactMethod
@@ -21,6 +31,38 @@ public class ReactNativeFirebaseGtm extends ReactContextBaseJavaModule{
     @ReactMethod
     public void setUserProperty(String name, String value) {
         FirebaseAnalytics.getInstance(getReactApplicationContext()).setUserProperty(name, value);
+    }
+
+    /**
+     * Fetch remote config value from Firebase
+     * @param cacheTime cache time in seconds, default: 43200L, which is 12 hours
+     */
+    @ReactMethod
+    public void fetchRemoteConfig(long cacheTime) {
+        cacheTime = (cacheTime == 0 ? 43200L: cacheTime);
+        mFirebaseRemoteConfig.fetch(cacheTime)
+                .addOnCompleteListener(this.getCurrentActivity(), new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            // After config data is successfully fetched, it must be activated before newly fetched
+                            // values are returned.
+                            mFirebaseRemoteConfig.activateFetched();
+                            Log.d(TAG, "Fetched remote config.");
+                        } else {
+                            Log.e(TAG, "Fetch remote config: failed.");
+                        }
+                    }
+                });
+    }
+
+    /**
+     * Return the remote config value for given key
+     * @param key The name of the key
+     */
+    @ReactMethod
+    public String getRemoteConfigValue(String key) {
+        return mFirebaseRemoteConfig.getString(key);
     }
 
     @Override
