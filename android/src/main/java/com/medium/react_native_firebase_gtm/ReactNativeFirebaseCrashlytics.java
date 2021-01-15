2e7d14b197781fa0dd94be7ba74c4828f1e170d6
Lible^Niel <daniel.yanuar@reebonz.com>
2021-01-15 17:52:37 +0700

    Create custom Reebonz exception to separate some non fatal error

--- android/src/main/java/com/medium/react_native_firebase_gtm/ReactNativeFirebaseCrashlytics.java
+++ android/src/main/java/com/medium/react_native_firebase_gtm/ReactNativeFirebaseCrashlytics.java
@@ -46,6 +46,30 @@ public class ReactNativeFirebaseCrashlytics extends ReactContextBaseJavaModule {
 
         ReebonzException e = new ReebonzException(message, code, domain);
         
+        switch (code) {
+            case -1000:
+                e = new ReebonzPaymentException(message, code, domain);
+                break;
+            case -999:
+                e = new ReebonzDeeplinkException(message, code, domain);
+                break;
+            case -998:
+                e = new ReebonzCodePushException(message, code, domain);
+                break;
+            case -997:
+                e = new ReebonzAPIException(message, code, domain);
+                break;
+            case -996:
+                e = new ReebonzNetworkException(message, code, domain);
+                break;
+            case -995:
+                e = new ReebonzShareException(message, code, domain);
+                break;
+            default:
+                e = new ReebonzTestingException(message, code, domain);
+                break;
+        }
+        
         mFirebaseCrashlytics.recordException(e);
     }
 
@@ -80,5 +104,108 @@ public class ReactNativeFirebaseCrashlytics extends ReactContextBaseJavaModule {
     @Override
     public String getName() {
         return "ReactNativeFirebaseCrashlytics";
+    }
+}
+
+class ReebonzException extends Exception {
+    private String domain;
+    private int code;
+
+    public ReebonzException(String message, int code, String domain) {
+        super(message);
+        this.setCode(code);
+        this.setDomain(domain);
+    }
+
+    public ReebonzException(String message, int code, String domain, Throwable cause) {
+        super(message, cause);
+        this.setCode(code);
+        this.setDomain(domain);
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+}
+
+class ReebonzPaymentException extends ReebonzException {
+    public ReebonzPaymentException(String message, int code, String domain) {
+        super(message, code, domain);
+    }
+
+    public ReebonzPaymentException(String message, int code, String domain, Throwable cause) {
+        super(message, code, domain, cause);
+    }
+}
+
+class ReebonzDeeplinkException extends ReebonzException {
+    public ReebonzDeeplinkException(String message, int code, String domain) {
+        super(message, code, domain);
+    }
+
+    public ReebonzDeeplinkException(String message, int code, String domain, Throwable cause) {
+        super(message, code, domain, cause);
+    }
+}
+
+class ReebonzCodePushException extends ReebonzException {
+    public ReebonzCodePushException(String message, int code, String domain) {
+        super(message, code, domain);
+    }
+
+    public ReebonzCodePushException(String message, int code, String domain, Throwable cause) {
+        super(message, code, domain, cause);
+    }
+}
+
+class ReebonzAPIException extends ReebonzException {
+    public ReebonzAPIException(String message, int code, String domain) {
+        super(message, code, domain);
+    }
+
+    public ReebonzAPIException(String message, int code, String domain, Throwable cause) {
+        super(message, code, domain, cause);
+    }
+}
+
+class ReebonzNetworkException extends ReebonzException {
+    public ReebonzNetworkException(String message, int code, String domain) {
+        super(message, code, domain);
+    }
+
+    public ReebonzNetworkException(String message, int code, String domain, Throwable cause) {
+        super(message, code, domain, cause);
+    }
+}
+
+class ReebonzShareException extends ReebonzException {
+    public ReebonzShareException(String message, int code, String domain) {
+        super(message, code, domain);
+    }
+
+    public ReebonzShareException(String message, int code, String domain, Throwable cause) {
+        super(message, code, domain, cause);
+    }
+}
+
+class ReebonzTestingException extends ReebonzException {
+    public ReebonzTestingException(String message, int code, String domain) {
+        super(message, code, domain);
+    }
+
+    public ReebonzTestingException(String message, int code, String domain, Throwable cause) {
+        super(message, code, domain, cause);
     }
 }
